03457813d42dc288795cc429deeefabfa8c4fd0f
Pablo <delgadinop@gmail.com>
2022-08-25 20:17:12 -0300

    Crear Landing

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+
+const App = () => {
+
+  return (
+    <div className="App">
+       <Navbar />
+       <ItemListContainer />
+    </div>
+  )
+}
+
+export default App
